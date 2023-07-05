e5fae44882aaf5abb8a01c227a58c6633227e445
jagadeesh m <jagadeeshm778@gmail.com>
2023-07-05 14:04:59 +0530

    init

--- CaseSwap_9.js
+++ CaseSwap_9.js
@@ -1,7 +1,15 @@
 const swap_test = "The Quick Brown Fox".split("");
-
 const swap_case = (input) =>
   input.map((ele) =>
       ele.toLowerCase() === ele ? ele.toUpperCase() : ele.toLowerCase()
     ).join("");
 console.log(swap_case(swap_test));
+//using loop
+let res="";
+for(let i=0;i<swap_test.length;i++){
+    if(swap_test[i]==swap_test[i].toLowerCase())
+      res+=swap_test[i];
+      else
+      res+=swap_test[i].toUpperCase();
+}
+console.log(res);
